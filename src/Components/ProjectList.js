a8e152bbbf8f45392f09d3f11cf1f40625e5b831
Joydeep Das <joydeep.accounts@tuta.io>
2023-03-13 02:09:03 +0530

    done for today

---
+++ src/Components/ProjectList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../CSS/ProjectList.css";
+
+function ProjectList() {
+    return (
+        <div className="ProjectList">
+        </div>
+    )
+}
+
+export default ProjectList;
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import ProjectList from '../Components/ProjectList';
+
+import taskManager from "../Image/task-manager-react.png";
+
+import {FaReact} from 'react-icons';
+
 
 import "../CSS/Projects.css";
 
 function Projects() {
     return (
         <div className="Projects">
+
+            <div>
+                <div><img src={taskManager} alt="Task Manager" /></div>
+                <div>
+                    <h4>Task Manager</h4>
+                </div>
+
+                <img src={FaReact} alt="" />
+
+                
+
+            </div>
+
         </div>
     )
 };
