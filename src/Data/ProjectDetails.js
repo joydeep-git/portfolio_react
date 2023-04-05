152538a57198dd811050b37f067eff5ea4b71f46
JOYDEEP <joydeep.accounts@tuta.io>
2023-04-05 10:07:11 +0530

    x

--- src/Data/ProjectDetails.js
+++ src/Data/ProjectDetails.js
@@ -13,28 +13,32 @@ import weatherApp from "../Image/weather-app.png";
 
 export const projectDetails = [
 
-    { id:1, name: "Task Manager" , image: taskManager, techUsed: "React.js  & Firebase", github: "https://github.com/joydeep-git/task-manager_react", liveUrl: "https://jd-taskmanager-react.netlify.app/",},
+    // { id:1, name: "Task Manager" , image: taskManager, techUsed: "React.js  & Firebase", github: "https://github.com/joydeep-git/task-manager_react", liveUrl: "https://jd-taskmanager-react.netlify.app/",},
 
-    { id: 2 , name: " To-Do List" , image: toDoList, github: "https://github.com/joydeep-git/to-do-list_react", liveUrl: "https://jd-todolist-react.netlify.app/", techUsed: "React.js"},
+    // { id:2, name: "Task Manager" , image: taskManager, techUsed: "React.js  & Firebase", github: "https://github.com/joydeep-git/task-manager_react", liveUrl: "https://jd-taskmanager-react.netlify.app/",},
 
-    { id: 3, name: "Weather App " , image: weatherApp, github: "https://github.com/joydeep-git/weather-app_js", liveUrl: "https://jd-weatherapp-js.netlify.app/", techUsed:" HTML, CSS & JS", },
+    { id:3, name: "Task Manager" , image: taskManager, techUsed: "React.js  & Firebase", github: "https://github.com/joydeep-git/task-manager_react", liveUrl: "https://jd-taskmanager-react.netlify.app/",},
 
-    { id: 4, name: "Form Validation", image: formValidation, github: "https://github.com/joydeep-git/Form-Validation_js", liveUrl: "https://jd-formvalidation-js.netlify.app/", techUsed:" HTML, CSS & JS",},
+    { id: 4 , name: " To-Do List" , image: toDoList, github: "https://github.com/joydeep-git/to-do-list_react", liveUrl: "https://jd-todolist-react.netlify.app/", techUsed: "React.js"},
 
-    { id:  5, name: "Rock Paper Scissor", image: rockPaper, github: "https://github.com/joydeep-git/rock-paper-scissor_js", liveUrl: "https://jd-rockpaperscissor-js.netlify.app/", techUsed:" HTML, CSS & JS",},
+    { id: 5, name: "Weather App " , image: weatherApp, github: "https://github.com/joydeep-git/weather-app_js", liveUrl: "https://jd-weatherapp-js.netlify.app/", techUsed:" HTML, CSS & JS", },
 
-    { id:  6, name: "Calculator App", image: calculator,  github: "https://github.com/joydeep-git/calculator-app_js", liveUrl: "https://jd-calculator-js.netlify.app/" , techUsed:" HTML, CSS & JS",},
+    { id: 6, name: "Form Validation", image: formValidation, github: "https://github.com/joydeep-git/Form-Validation_js", liveUrl: "https://jd-formvalidation-js.netlify.app/", techUsed:" HTML, CSS & JS",},
 
-    { id: 7 , name: "Number Game", image: guessNumber, github: "https://github.com/joydeep-git/guess-the-number_js", liveUrl: "https://jd-guessthenumber-js.netlify.app/", techUsed:" HTML, CSS & JS",},
+    { id:  7, name: "Rock Paper Scissor", image: rockPaper, github: "https://github.com/joydeep-git/rock-paper-scissor_js", liveUrl: "https://jd-rockpaperscissor-js.netlify.app/", techUsed:" HTML, CSS & JS",},
+
+    { id:  8, name: "Calculator App", image: calculator,  github: "https://github.com/joydeep-git/calculator-app_js", liveUrl: "https://jd-calculator-js.netlify.app/" , techUsed:" HTML, CSS & JS",},
+
+    { id: 9 , name: "Number Game", image: guessNumber, github: "https://github.com/joydeep-git/guess-the-number_js", liveUrl: "https://jd-guessthenumber-js.netlify.app/", techUsed:" HTML, CSS & JS",},
     
-    {id: 8,name: "Clipboard Page", image: clipboardLanding, github: "https://github.com/joydeep-git/clipboard-landing-page_css", liveUrl: "https://jd-clipboardpage-css.netlify.app/", techUsed:" HTML & CSS",},
+    {id: 10,name: "Clipboard Page", image: clipboardLanding, github: "https://github.com/joydeep-git/clipboard-landing-page_css", liveUrl: "https://jd-clipboardpage-css.netlify.app/", techUsed:" HTML & CSS",},
 
-    { id: 9, name: "Fylo Webpage", image: fyloLanding, github: "https://github.com/joydeep-git/fylo-landing-page_css", liveUrl: "https://jd-fylopage-css.netlify.app/", techUsed:" HTML & CSS",},
+    { id: 11, name: "Fylo Webpage", image: fyloLanding, github: "https://github.com/joydeep-git/fylo-landing-page_css", liveUrl: "https://jd-fylopage-css.netlify.app/", techUsed:" HTML & CSS",},
 
-    { id: 10, name: "Chat App", image: chatApp,  github: "https://github.com/joydeep-git/chat-app_css", liveUrl: "https://jd-chatapp-css.netlify.app/", techUsed:" HTML & CSS",},
+    { id: 12, name: "Chat App", image: chatApp,  github: "https://github.com/joydeep-git/chat-app_css", liveUrl: "https://jd-chatapp-css.netlify.app/", techUsed:" HTML & CSS",},
 
-    { id: 11 , name: "Order Summary", image: orderSummary, github: "https://github.com/joydeep-git/order-summary_css", liveUrl: "https://jd-ordersummary-css.netlify.app/",techUsed:" HTML & CSS",},
+    { id: 13 , name: "Order Summary", image: orderSummary, github: "https://github.com/joydeep-git/order-summary_css", liveUrl: "https://jd-ordersummary-css.netlify.app/",techUsed:" HTML & CSS",},
 
-    { id: 12 , name: "Testimonial Page " , image: testimonialGrid, github: "https://github.com/joydeep-git/testimonials-grid_css", liveUrl: "https://jd-testimonialgrid-css.netlify.app/",techUsed:" HTML & CSS",}
+    { id: 14 , name: "Testimonial Page " , image: testimonialGrid, github: "https://github.com/joydeep-git/testimonials-grid_css", liveUrl: "https://jd-testimonialgrid-css.netlify.app/",techUsed:" HTML & CSS",}
 
 ];
